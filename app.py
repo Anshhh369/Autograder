d76f0eacb3bd2cd2a723037e0ce7c0bd1c09442f
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-09-20 16:24:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -34,13 +34,17 @@ def extract_answers(text,pattern):
     
 
     if search_result:
-        # Extract the question and answer from the matched groups
-        question = search_result.group(1).strip()
+
+        while search_result:
+            # Extract the question and answer from the matched groups
+            question = search_result.group(1).strip()
+
+            extracted_answers.append(question)
         
-        answer = search_result.group(2).strip()
-        answers_cleaned = re.sub(r'(^#)', r'\\#', answer, flags=re.MULTILINE)
+            answer = search_result.group(2).strip()
+            answers_cleaned = re.sub(r'(^#)', r'\\#', answer, flags=re.MULTILINE)
         
-        extracted_answers.append(answers_cleaned)
+            extracted_answers.append(answers_cleaned)
         
     else:
         extracted_answers = st.write("Answers not found")
