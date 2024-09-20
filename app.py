b54a6951fe62af466f0ba387cc5ff6443a22b0e2
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-09-20 15:57:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def extract_answers(text):
     for i in range(len(questions)):
         extracted_questions = match.questions(i).strip()  # Question on its own line
         if i < len(answers):
-            extracted_answers = match.answers_cleane(i).strip()  # Answer on the next line 
+            extracted_answers = match.answers_cleaned(i).strip()  # Answer on the next line 
         else:
             extracted_answers = "Answer not found"
             
