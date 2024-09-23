b368f90be6ef4eb6315c99b6b719e8e51a9966b4
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-09-23 15:52:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def extract_text_from_file(uploaded_file):
         return None
 
     
-
+    doc = Document(page_content=file_content)
     
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     docs = text_splitter.split_documents(doc)
