af8bc8f6e056c8e6d43cc02c746504de062ed65d
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-09-24 11:06:33 -0400

    Update vectordb.py

--- vectordb.py
+++ vectordb.py
@@ -2,7 +2,7 @@ azure_api_key = secrets["azure"]["api_key"]
 os.environ["AZURE_API_KEY"] = azure_api_key
 
 vector_store_address = "https://ragservices.search.windows.net"
-vector_store_password = "IVMAEF98D6Tn8w4eQ53VstzUHXfelrAJn4sBPlY8hZAzSeByAPxr"
+vector_store_password = "azure_api_key"
 index_name = "autograder-vectordb"
 
 OpenAIEmbeddings = OpenAIEmbeddings(openai_api_key=openai_api_key, model=model)
