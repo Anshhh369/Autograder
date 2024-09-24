46ac805cef63c20defe89fbb179e0a83d2e427f8
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-09-24 11:05:05 -0400

    Update vectordb.py

--- vectordb.py
+++ vectordb.py
@@ -1,1 +1,16 @@
+def vector_store():
+  
+  vector_store = AzureSearch(
+        azure_search_endpoint=vector_store_address,
+        azure_search_key=vector_store_password,
+        index_name=index_name,
+        api_version = "2024-05-01-preview",
+        embedding_function=OpenAIEmbeddings.embed_query,
+        # Configure max retries for the Azure client
+        additional_search_client_options={"retry_total": 4},
+    )
 
+    db = vector_store.add_documents(documents=documents)
+
+  return db
+        
