d2742e0cc33a6efab0d7bc7e9e9a33ef361c8896
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-09-24 11:08:36 -0400

    Update vectordb.py

--- vectordb.py
+++ vectordb.py
@@ -1,13 +1,21 @@
 import os
 from langchain_community.vectorstores import AzureSearch
-from langchain_openai import AzureOpenAIEmbeddings, OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
+
+
+secrets = st.secrets  # Accessing secrets (API keys) stored securely
+
+openai_api_key = secrets["openai"]["api_key"]  # Accessing OpenAI API key from secrets
+os.environ["OPENAI_API_KEY"] = openai_api_key  # Setting environment variable for OpenAI API key
 
 azure_api_key = secrets["azure"]["api_key"]
 os.environ["AZURE_API_KEY"] = azure_api_key
 
 vector_store_address = "https://ragservices.search.windows.net"
 vector_store_password = "azure_api_key"
+
 index_name = "autograder-vectordb"
+model = "text-embedding-ada-002"
 
 OpenAIEmbeddings = OpenAIEmbeddings(openai_api_key=openai_api_key, model=model)
 
