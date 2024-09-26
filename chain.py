f36488eb47bf5ce7c63eac0aeaf72fc271cc58d8
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-09-26 00:37:52 -0400

    Update chain.py

--- chain.py
+++ chain.py
@@ -1,5 +1,7 @@
-system_prompt = """
-
+def get_chain(assignment,predefined_rubrics,example):
+        
+        system_prompt = """
+        
         You are an expert grader. Your job is to grade students assignment based on predifined rubrics.
 
         Start by greeting the user respectfully, collect the name of the user.
@@ -10,22 +12,46 @@ system_prompt = """
         
         Lastly, ask user if you want any modification or adjustments to the scores generated? If the user says no then end the conversation.
         
-
-
         
         """
 
+        prompt = ChatPromptTemplate.from_messages(
+                [("system", system_prompt), ("human", "{input}")]
+        )
 
+        prompt.format_messages(input = "query", assignment = "st.session_state.vector_store", example = "st.session_state.example", predefined_rubrics = "st.session_state.rubrics")
 
+        model_name = "gpt-4"
+        llm = ChatOpenAI(model_name=model_name)
 
-prompt = ChatPromptTemplate.from_messages(
-        [("system", system_prompt), ("human", "{input}")]
-    )
+        chain = LLMChain(llm=llm, prompt=prompt)
 
-    prompt.format_messages(input = "query", assignment = "st.session_state.vector_store", options = "st.session_state.selected_option", context = "st.session_state.context", chat_history = "st.session_state.chat_history")
+        if st.session_state.vector_store:
+        
+                retriever = AzureAISearchRetriever(
+                    content_key="assignment", 
+                    top_k=1, 
+                    index_name="autograder-vectordb",
+                )
+                chain = create_retrieval_chain(retriever, chain)
 
-    model_name = "gpt-4"
-    llm = ChatOpenAI(model_name=model_name)
+                st.session_state.chat_active = True
 
-    chain = LLMChain(llm=llm, prompt=prompt)
+                st.session_state.chain = chain
+
+                return st.session_state.chain
+
+def get_scores():
+        
+        chains = get_chain(st.session_state.vector_store,st.session_state.predefined_rubrics,st.session_state.example)
+        response = chains.invoke({"input": query, "assignment": st.session_state.vector_store,"example" : st.session_state.example, "predefined_rubrics": st.session_state.rubrics})
+        
+        try:
+                answer = response['text']
+                
+        except:
+                ans = response['answer']
+                answer = ans['text']
+              
+        return answer
 
