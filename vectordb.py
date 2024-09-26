0039fd09cbc28a2c515eb0a35fa4ffbc4cf4cdd4
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-09-25 23:06:40 -0400

    Update vectordb.py

--- vectordb.py
+++ vectordb.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import os
-from langchain_community.vectorstores import AzureSearch
+from langchain_community.vectorstores.azuresearch import AzureSearch
 from langchain_openai import OpenAIEmbeddings
 
 
@@ -26,7 +26,7 @@ def vector_db(document):
     azure_search_endpoint=vector_store_address,
     azure_search_key=vector_store_password,
     index_name=index_name,
-    api_version = "2024-05-01-preview",
+    api_version = "2020-08-01",
     embedding_function=OpenAIEmbeddings.embed_query,
     # Configure max retries for the Azure client
     additional_search_client_options={"retry_total": 4},
