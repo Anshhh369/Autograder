3615a4606a50b8667ca8c3cab2c65acbc1a60528
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-09-26 00:27:02 -0400

    Update rubrics.py

--- rubrics.py
+++ rubrics.py
@@ -1,1 +1,30 @@
 
+def rubrics():
+
+  predefined_rubrics = """
+  
+  predifined rubrics - 
+  Problem Definition and Understanding (20%):
+  Not clear about the problem (0-4 points)
+  Somewhat clear about the problem (5-9 points)
+  Clear about the problem, minor errors (10-14 points)
+  Very clear about the problem, no errors (15-20 points)
+  Technical Accuracy (40%):
+  Many technical inaccuracies (0-16 points)
+  Several technical inaccuracies (17-24 points)
+  Few technical inaccuracies (25-32 points)
+  No technical inaccuracies (33-40 points)
+  Coding/Programming Skills (20%):
+  Many coding errors, does not compile/run (0-4 points)
+  Several coding errors, compiles/runs with issues (5-9 points)
+  Few coding errors, compiles/runs with minor issues (10-14 points)
+  No coding errors, compiles/runs smoothly (15-20 points)
+  Problem-solving Approach (20%):
+  No clear approach to solving the problem (0-4 points)
+  A somewhat clear approach, but with significant errors (5-9 points)
+  A clear approach with minor errors (10-14 points)
+  A clear and correct approach (15-20 points)
+
+  """
+
+  return predefined_rubrics
