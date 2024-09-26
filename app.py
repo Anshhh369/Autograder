9ed86cffe2d3715199ce69d6af264071f29de8f2
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-09-26 14:14:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ if uploaded_file:
 
     st.session_state.uploaded_file = file_content
 
-    Extract answers using regex patterns
+    # Extract answers using regex patterns
     extracted_answers = extract_answers(st.session_state.uploaded_file,pattern)
 
     st.write("Extracted Answers:", extracted_answers)
