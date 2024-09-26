62db3b0df89bc633d787f64b82514d49f1239d2f
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-09-26 14:03:02 -0400

    Update document_processing.py

--- document_processing.py
+++ document_processing.py
@@ -49,54 +49,54 @@ def process_document(uploaded_file):
 
 
 
-# # Function to extract answers using regex patterns
-# def extract_answers(uploaded_file,pattern):
+# Function to extract answers using regex patterns
+def extract_answers(uploaded_file,pattern):
 
-#     file_details = {"filename": uploaded_file.name, "filetype": uploaded_file.type}
+    file_details = {"filename": uploaded_file.name, "filetype": uploaded_file.type}
     
-#     # Save file to a temporary directory
-#     temp_dir = tempfile.mkdtemp()
-#     path = os.path.join(temp_dir, uploaded_file.name)
+    # Save file to a temporary directory
+    temp_dir = tempfile.mkdtemp()
+    path = os.path.join(temp_dir, uploaded_file.name)
         
-#     with open(path, "wb") as f:
-#         f.write(uploaded_file.getvalue())
+    with open(path, "wb") as f:
+        f.write(uploaded_file.getvalue())
 
-#     # Determine file extension
-#     file_extension = uploaded_file.name.split(".")[-1].lower()
+    # Determine file extension
+    file_extension = uploaded_file.name.split(".")[-1].lower()
     
-#     if file_extension == "txt":
-#         text = uploaded_file.read().decode('utf-8')
-#     elif file_extension == "pdf":
-#         with pdfplumber.open(uploaded_file) as pdf:
-#             pages = [page.extract_text() for page in pdf.pages]
-#             text = "\n".join(pages)
-#     elif file_extension == "docx":
-#         pages = docx.Document(uploaded_file)
-#         text = "\n".join([para.text for para in pages.paragraphs])
+    if file_extension == "txt":
+        text = uploaded_file.read().decode('utf-8')
+    elif file_extension == "pdf":
+        with pdfplumber.open(uploaded_file) as pdf:
+            pages = [page.extract_text() for page in pdf.pages]
+            text = "\n".join(pages)
+    elif file_extension == "docx":
+        pages = docx.Document(uploaded_file)
+        text = "\n".join([para.text for para in pages.paragraphs])
 
 
-#     extracted_answers = []
+    extracted_answers = []
 
-#     # Pattern
-#     pattern = r"(Question\s*\d:.*?)(Answer\s*\d:.*)"
+    # Pattern
+    pattern = r"(Question\s*\d:.*?)(Answer\s*\d:.*)"
     
-#     # Use re.search to iterate through the matches
-#     search_result = re.search(pattern, text, re.DOTALL)
+    # Use re.search to iterate through the matches
+    search_result = re.search(pattern, text, re.DOTALL)
     
     
-#     if search_result:
-#         # Extract the question and answer from the matched groups
-#         question = search_result.group(1).strip()
+    if search_result:
+        # Extract the question and answer from the matched groups
+        question = search_result.group(1).strip()
 
-#         extracted_answers.append(question)
+        extracted_answers.append(question)
         
-#         answer = search_result.group(2).strip()
-#         answers_cleaned = re.sub(r'(^#)', r'\\#', answer, flags=re.MULTILINE)
+        answer = search_result.group(2).strip()
+        answers_cleaned = re.sub(r'(^#)', r'\\#', answer, flags=re.MULTILINE)
         
-#         extracted_answers.append(answers_cleaned)
+        extracted_answers.append(answers_cleaned)
         
-#     else:
+    else:
         
-#         extracted_answers = st.write("Answers not found")
+        extracted_answers = st.write("Answers not found")
             
-#     return extracted_answers
+    return extracted_answers
