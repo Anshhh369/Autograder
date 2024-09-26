904198fb7cce236d64610f414b44a835fde59c9e
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-09-26 13:50:59 -0400

    Update chain.py

--- chain.py
+++ chain.py
@@ -66,7 +66,7 @@ def get_chain(assignment,predefined_rubrics,example,chat_history):
 def get_scores(query):
         
         chains = get_chain(st.session_state.vector_store,st.session_state.rubrics,st.session_state.example,st.session_state.chat_history)
-        response = chains.invoke({"input": query, "assignment": st.session_state.vector_store,"example" : st.session_state.example, "predefined_rubrics": st.session_state.rubrics,"chat_history": st.session_state.chat_history})
+        response = chains.invoke({"input": query, "assignment": st.session_state.vector_store, "example" : st.session_state.example, "predefined_rubrics": st.session_state.rubrics,"chat_history": st.session_state.chat_history})
         
         try:
                 answer = response['text']
