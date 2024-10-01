c3c4270a88e781c2bbe0b36f92b98471cbbd9110
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-10-01 16:38:06 -0400

    Update vectordb.py

--- vectordb.py
+++ vectordb.py
@@ -27,7 +27,7 @@ model = "text-embedding-ada-002"
 
 OpenAIEmbeddings = OpenAIEmbeddings(openai_api_key=openai_api_key, model=model)
 
-def vector_db():
+def vector_db(query):
   vector_store = AzureSearch(
     azure_search_endpoint=vector_store_address,
     azure_search_key=vector_store_password,
@@ -38,20 +38,16 @@ def vector_db():
     additional_search_client_options={"retry_total": 4},
   )
 
-  query = st.session_state.user_name
-  
-  if query:
-
-    docs = vector_store.similarity_search(
-      query=query,
-      k=37, 
-      search_type="similarity"
-    )
+  docs = vector_store.similarity_search(
+    query=query,
+    k=37, 
+    search_type="similarity"
+  )
     
-    content = []
-    for doc in docs:
-      document = doc.page_content
-      content.append(document)
+  content = []
+  for doc in docs:
+    document = doc.page_content
+    content.append(document)
 
     # pattern = r""
 
@@ -61,9 +57,8 @@ def vector_db():
     #   if search_result:
     #     result = search_result.group(0)
     
-    st.write("result: ",content)
-    return content
+  st.write("result: ",content)
+  
+  return content
 
-  else:
-    return None
         
