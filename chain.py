5ebd0107bd83b20bf92becd6af685690cb523c52
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-10-01 16:10:16 -0400

    Update chain.py

--- chain.py
+++ chain.py
@@ -23,7 +23,7 @@ def get_chain(assignment,predefined_rubrics,chat_history):
         
         You are an expert grader, your name is AutoGrader. Your job is to grade {assignment} based on {predefined_rubrics}.
 
-        Start by greeting the user respectfully, collect the name of the user. 
+        Start by greeting the user respectfully, collect the name of the user. Save the name of the user in a variable called user_name.
         After that verify {predefined_rubrics} with the user by displaying whole exact rubric to them clearly.
         Move to the next step only after successfully verifying.
         Next step is to grade the assignment, go through the {assignment} and highlight the mistakes that user made, make sure you explain all the mistakes in detail with soultions.
