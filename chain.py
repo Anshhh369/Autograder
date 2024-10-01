7069e8c2890e1d918a5ae242f68b69531b03cf9e
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-10-01 16:18:04 -0400

    Update chain.py

--- chain.py
+++ chain.py
@@ -70,7 +70,7 @@ def get_scores(query):
         pattern = r"user_name\s*=\s*(\w+)"
         search_results = re.search(pattern, answer, re.DOTALL)
         if search_results:
-                result = search_results.group(2)
+                result = search_results.group(1)
 
                 st.write("result: ",result)
 
