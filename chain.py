1b6092d5f436475019ae35f9698862c7c5bdf1b3
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-10-01 01:10:36 -0400

    Update chain.py

--- chain.py
+++ chain.py
@@ -45,17 +45,7 @@ def get_chain(assignment,predefined_rubrics,chat_history):
         llm = ChatOpenAI(model_name=model_name)
         
 
-        chain = LLMChain(llm = llm,prompt = prompt)
-
-        retriever = AzureAISearchRetriever(
-                content_key="content",
-                top_k=1,
-                index_name="predefined_rubrics",
-        )
-
-                
-        retrieval_chain = create_retrieval_chain(retriever, chain)
-        st.session_state.chain = retrieval_chain
+        st.session_state.chain = LLMChain(llm = llm,prompt = prompt)
 
         st.session_state.chat_active = True
 
