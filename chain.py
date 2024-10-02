e4cefaa2b4de6364f4177c208520156982831460
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-10-01 21:31:39 -0400

    Update chain.py

--- chain.py
+++ chain.py
@@ -28,7 +28,8 @@ def get_chain(assignment,predefined_rubrics,chat_history):
         
         Only after verifying the name, verify {predefined_rubrics} with the user by displaying whole exact {predefined_rubrics} to them clearly.
         Only after successfully verifying predefined_rubrics, move to the next step.
-        Next step is to grade the assignment, go through the {assignment} and highlight the mistakes that user made, make sure you explain all the mistakes in detail with soultions.
+        Next step is to ask the user to upload the assignment through navigating to uploag page from left hand side. 
+        Go through the {assignment} and highlight the mistakes that user made, make sure you explain all the mistakes in detail with soultions.
         Be consistent with the scores and feedbacks generated.
         Lastly, ask user if they want any modification or adjustments to the scores generated, if user says no then end the conversation.
 
