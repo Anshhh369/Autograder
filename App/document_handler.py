65c07d02e492536b428e5486fa4ac5729a96a3d6
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-10-03 13:56:13 -0400

    Update document_handler.py

--- App/document_handler.py
+++ App/document_handler.py
@@ -10,42 +10,6 @@ import pdfplumber
 import docx
 import streamlit as st
 
-# Function to extract text from uploaded files
-def process_document(uploaded_file):
-    
-    file_details = {"filename": uploaded_file.name, "filetype": uploaded_file.type}
-    
-    # Save file to a temporary directory
-    temp_dir = tempfile.mkdtemp()
-    path = os.path.join(temp_dir, uploaded_file.name)
-        
-    with open(path, "wb") as f:
-        f.write(uploaded_file.getvalue())
-
-    # Determine file extension
-    file_extension = uploaded_file.name.split(".")[-1].lower()
-
-    # Load document based on its extension
-    
-    if file_extension == "txt":
-        loader = TextLoader(path)
-    elif file_extension == "pdf":
-        loader = PyPDFLoader(path)
-    elif file_extension == "docx":
-        loader = Docx2txtLoader(path)     
-    else:
-        st.error("Unsupported file type.")
-        return None
-
-    # Load documents and split text
-    docs = loader.load()
-    
-    text_splitter =  RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    documents = text_splitter.split_documents(docs)
-
-    return documents     
-
-
 
 # Function to extract answers using regex patterns
 def extract_answers(uploaded_file):
